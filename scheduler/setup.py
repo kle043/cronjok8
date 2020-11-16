d719a9f5fb65ed9d565e72e31701b379407acc06
kle043 <19959498+kle043@users.noreply.github.com>
2020-11-16 13:18:00 +0100

    add scheduler code

---
+++ scheduler/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+NAME = 'scheduler'
+
+setup(
+    name=NAME,
+    version='1.0.0',
+    description='A worker',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=['requests==2.21.0', 'kubernetes==12.0.1', 'pyyaml==5.3.1'],
+    entry_points={
+        "console_scripts": [
+            f"{NAME} = {NAME}.__main__:main"
+        ]
+    },
+
+)
