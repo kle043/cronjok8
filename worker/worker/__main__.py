f3fe74ad4fe79015acf38ef14a7f36dd45a551d0
kle043 <19959498+kle043@users.noreply.github.com>
2020-11-18 08:58:50 +0100

    skip sleep to get logsearlier

--- worker/worker/__main__.py
+++ worker/worker/__main__.py
@@ -7,6 +7,7 @@ JOB_SERVER_BASE_URL = os.environ['JOB_SERVER']
 
 def main(args=None):
 
+    print('Requesting data')
     parser = argparse.ArgumentParser()
     parser.add_argument('job_id', type=int, help='A job id')
 
@@ -15,7 +16,8 @@ def main(args=None):
     res = requests.post(f'{JOB_SERVER_BASE_URL}/worktime', json = {'id': int(args.job_id)})
     worker = res.json()
     print(f'Worker {worker["name"]} starts working!')
-    time.sleep(worker['worktime']*60)
+    for _ in range(0, worker['worktime']):
+        print(f'{worker["name"]} working!')
     print('Finished!')
 
 if __name__ == "__main__":
